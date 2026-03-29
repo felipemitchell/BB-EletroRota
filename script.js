dc0ee54f865271a113dc6233dac65c5eead07c58
EdsonAguiar888 <edsonaguiar888@gmail.com>
2026-03-29 15:34:36 -0300

    Adicionando eventos de click aos cards

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 // codigos js
+
+
+
+
+
+
+
+
+
+
+
+const estacoes =  document.getElementById("estacoes")
+
+
+
+estacoes.addEventListener("click", () =>{
+    alert("Encontre aqui as estacoes")
+    
+})
+
+autonomia.addEventListener("click", () => {
+    alert("Informações sobre autonomia...")
+} )
+
+viagem.addEventListener("click", () =>{
+    alert("Planejar Viagem")
+})
+
+cadastro.addEventListener("click", () =>{
+    alert("Cadastre aqui seu carro")
+})
+
+
+
+
+
+// function estacoes(){
+//     alert("Clicou")
+// }
